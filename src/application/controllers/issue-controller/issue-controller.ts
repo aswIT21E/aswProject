cebb6f8dfb69bc3c582e0f0e4846b35962de19bc
quimvi <quim.baget@gmail.com>
2023-03-23 22:24:15 +0100

    Add routes to get HTML pages

--- src/application/controllers/issue-controller/issue-controller.ts
+++ src/application/controllers/issue-controller/issue-controller.ts
@@ -1,7 +1,7 @@
-import type { Request, Response } from "express";
+import type { Request, Response } from 'express';
 
-import type { IIssue } from "~/domain/entities/issue";
-import { IssueRepository } from "~/domain/repositories/issue-repository/issue-repository";
+import type { IIssue } from '~/domain/entities/issue';
+import { IssueRepository } from '~/domain/repositories/issue-repository/issue-repository';
 
 export class IssueController {
   public static async createIssue(req: Request, res: Response): Promise<void> {
@@ -10,7 +10,7 @@ export class IssueController {
       await IssueRepository.addIssue(issue);
       res.status(200);
       res.json({
-        message: "issue created",
+        message: 'issue created',
       });
     } catch (e) {
       res.status(500);
@@ -22,7 +22,7 @@ export class IssueController {
 
   public static async getAllIssues(
     _req: Request,
-    res: Response
+    res: Response,
   ): Promise<void> {
     try {
       const issues: IIssue[] = await IssueRepository.getAllIssues();
@@ -36,5 +36,19 @@ export class IssueController {
         error: e,
       });
     }
+  }
+
+  public static async getNewIssuePage(
+    _req: Request,
+    res: Response,
+  ): Promise<void> {
+    res.sendFile('views/addIssue.html', { root: 'src' });
+  }
+
+  public static async getIssuePage(
+    _req: Request,
+    res: Response,
+  ): Promise<void> {
+    res.sendFile('views/issue.html', { root: 'src' });
   }
 }
